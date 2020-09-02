ae65aa24abe6f9a1b4f2941f6be174585d2634ed
Bobby Neal <rsneal01@gmail.com>
2020-09-02 16:36:13 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY key,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER
+);
+
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Bubby', 29, 'male', 'white', 'phlegmatic', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Schnooky', 3, 'male', 'black', 'choleric', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Cuddles', 1, 'male', 'mixed', 'sanguine', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Jazzy', 7, 'female', 'mixed', 'melancholic', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Hanner', 29, 'female', 'white', 'melancholic', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Noodle', 1, 'male', 'mixed', 'phlegmatic', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Jackie boy', 16, 'male', 'brown', 'choleric', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 15, 'male', 'white', 'melancholic', 1);
